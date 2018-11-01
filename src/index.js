9302d1d1130f0abf77d518a74a79a7c8eb437b03
Andy <andyfilchev@gmail.com>
2018-11-01 16:01:41 +0200

    feat: Connect index.js to redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import configureStore from './store/configureStore';
+import App from './components/App';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
+
 registerServiceWorker();
