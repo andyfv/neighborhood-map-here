6608395820342b4f601d32e517d47ee96d96db66
Andy <andyfilchev@gmail.com>
2018-11-10 21:59:02 +0200

    feat: Add debounce for delaying search requests

---
+++ src/utils/debounce.js
@@ -0,0 +1,10 @@
+export const _debounce = (callback, time) => {
+    let interval;
+    return (...args) => {
+        clearTimeout(interval);
+        interval = setTimeout(() => {
+            interval = null;
+            callback(...args);
+        }, time);
+    };
+};
