becea4f89b9f5593981922569775f19c5fc4aef0
Andy <andyfilchev@gmail.com>
2018-11-10 21:38:04 +0200

    feat: Add map actions

--- src/actions/mapActions.js
+++ src/actions/mapActions.js
@@ -1,11 +1,29 @@
 import C from './actionTypes';
-import { loadMapLibs, initMap } from '../utils/HereMapsAPI';
+import { errorCaught } from './errorActions';
+import { loadMapLibs, initMap, createInfoBubble } from '../utils/HereMapsAPI';
+
+export function loadMapSucceeded() {
+    return {
+        type: C.LOAD_MAP_SUCCEEDED
+    }
+}
 
 export function mapLoadAndInit(defaultLocation) {
-    return ((dispatch, getState) => 
-        loadMapLibs()
-        .then(() => initMap(defaultLocation))
-        // dispatch an action that will open a modal with the error message
-        .catch(error => dispatch()) 
-    )()
+    return (dispatch, getState) => {
+        return loadMapLibs()
+            .then(() => dispatch(loadMapSucceeded()))
+            .then(() => initMap(defaultLocation))
+            // dispatch an action that will open a modal with the error message
+            .catch(error => 
+                dispatch(errorCaught(`Loading map failed: (${error})`)))
+    }
+}
+
+export function openInfoBubble(index) {
+    return (dispatch, getState) => {
+        // Sometimes clicking on a border returns undefined and breaks the chain
+        if (index !== undefined) {
+            return Promise.resolve(createInfoBubble(getState().search.results[index]))
+        }
+    }
 }
