2c8345afe7b154b4abca61b9e91b595d5017ade6
Andy Filchev <andyfilchev@Andys-iMac.lan>
2019-06-03 19:34:14 +0300

    refactor : Change API links to protocol-relative

--- src/utils/HereMapsData.js
+++ src/utils/HereMapsData.js
@@ -8,17 +8,17 @@ export const APP_CODE = '_bSUHCICzRh-aNcScP_8pw';
     Here Maps Libraries:
  */
 export const LIBS = {
-    mapsjsCore: 'http://js.api.here.com/v3/3.0/mapsjs-core.js',
-    mapsjsService: 'http://js.api.here.com/v3/3.0/mapsjs-service.js',
-    mapjsEvents: 'http://js.api.here.com/v3/3.0/mapsjs-mapevents.js',
-    mapjsPlaces: 'http://js.api.here.com/v3/3.0/mapsjs-places.js',
-    mapjsUI: 'http://js.api.here.com/v3/3.0/mapsjs-ui.js'
+    mapsjsCore: '//js.api.here.com/v3/3.0/mapsjs-core.js',
+    mapsjsService: '//js.api.here.com/v3/3.0/mapsjs-service.js',
+    mapjsEvents: '//js.api.here.com/v3/3.0/mapsjs-mapevents.js',
+    mapjsPlaces: '//js.api.here.com/v3/3.0/mapsjs-places.js',
+    mapjsUI: '//js.api.here.com/v3/3.0/mapsjs-ui.js'
 };
 
 export const API = {
-    search: 'https://places.cit.api.here.com/places/v1/discover/search',
-    places: 'https://places.cit.api.here.com/places/v1/autosuggest',
-    explore: 'https://places.cit.api.here.com/places/v1/discover/explore',
-    around: 'https://places.cit.api.here.com/places/v1/discover/around',
-    browse: 'https://places.cit.api.here.com/places/v1/browse'
+    search: '//places.cit.api.here.com/places/v1/discover/search',
+    places: '//places.cit.api.here.com/places/v1/autosuggest',
+    explore: '//places.cit.api.here.com/places/v1/discover/explore',
+    around: '//places.cit.api.here.com/places/v1/discover/around',
+    browse: '//places.cit.api.here.com/places/v1/browse'
 };
